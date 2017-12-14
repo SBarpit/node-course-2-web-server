d7af696ebe11751d5f53677d385dd26edbe9a473
Arpit Srivastava <arpit9840@gmail.com>
2017-12-14 14:44:05 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const hbs=require('hbs');
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+app.get('/',(req,res)=>{
+//res.send('<h1>Hello Express</h1>');
+// res.send({
+// 	name:'Arpit',
+// 	likes:[
+// 	'Bikeing',
+// 	'NetSurfing']
+res.render('home.hbs',{
+	pageTitle:'Home Page',
+	mgs:'Wellcome to my website',
+	});
+
+});
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		pageTitle:'About Page',
+		});
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		error:'Page Not Found',
+		status_code:'404'
+	});
+});
+app.listen(1995,()=>{
+	console.log('Server is up on port no 1995');
+});
